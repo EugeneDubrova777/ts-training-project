4480aa3f873f553219711637e4244ebd221ab17a
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2024-02-04 12:26:59 +0300

    add map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// console.log(user);
+// const company = new Company();
+// console.log(company);
 
-console.log(user);
-console.log(company);
+const map = document.getElementById('map');
+
+new google.maps.Map(map!, {
+  zoom: 3,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  backgroundColor: 'black',
+});
